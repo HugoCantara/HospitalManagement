f854a2c8a95b51e8e6e582b9b3f99dad178cb9ca
Hugo Cântara <hugo.cantara@gmail.com>
2024-07-19 17:23:04 +0100

    Create Generic Repository

---
+++ HospitalManagement.Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>Interface Generic Repository</summary>
+namespace HospitalManagement.Repositories.Interfaces
+{
+    using System.Linq.Expressions;
+
+    public interface IGenericRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "");
+
+        T GetById(object id);
+
+        Task<T> GetByIdAsync(object id);
+
+        void Add(T entity);
+
+        Task<T> AddAsync(T entity);
+
+        void Update(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        void Delete(T entity);
+
+        Task<T> DeleteAsync(T entity);
+    }
+}
