1941dc1d2911ead5e1637c23bcdd7956a3d59179
Hugo Cântara <hugo.cantara@gmail.com>
2024-07-19 17:39:17 +0100

    Create Unit Of Work

---
+++ HospitalManagement.Repositories/Implementation/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>Unit Of Work Class</summary>
+namespace HospitalManagement.Repositories.Implementation
+{
+    using HospitalManagement.Repositories.Interfaces;
+
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationDbContext _context;
+        private bool disposed = false;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing) 
+        {
+            if (!this.disposed) 
+            {
+                if (disposing) 
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public IGenericRepository<T> GenericRepository<T>() where T : class
+        {
+            IGenericRepository<T> repo = new GenericRepository<T>(_context);
+            return repo;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ HospitalManagement.Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>Unit Of Work Interface</summary>
+namespace HospitalManagement.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<T> GenericRepository<T>() where T : class;
+        void Save();
+    }
+}
